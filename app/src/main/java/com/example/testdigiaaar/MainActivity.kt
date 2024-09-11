1fbac2942bf14e829f125202448697d93a6034db
Vivek Singh <viveksingh@digia.tech>
2024-09-11 23:20:55 +0530

    Remove unused imports

--- app/src/main/java/com/example/testdigiaaar/MainActivity.kt
+++ app/src/main/java/com/example/testdigiaaar/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.testdigiaaar
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +10,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.testdigiaaar.ui.theme.TestDigiaAarTheme
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
